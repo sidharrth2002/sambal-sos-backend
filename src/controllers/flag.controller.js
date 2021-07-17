0c7d29ba75f9ab849f1aaafa104dbb547c35110b
Ronald545 <spikyron@gmail.com>
2021-07-17 13:16:52 +0800

    added docs

--- src/controllers/flag.controller.js
+++ src/controllers/flag.controller.js
@@ -4,6 +4,61 @@ const sequelize = require("sequelize");
 const moment = require("moment");
 
 // TODO: only send back reports made in the past week
+/**
+ * @swagger
+ *
+ *  paths:
+ *    /api/flag/getall:
+ *      get:
+ *        description: Get All flags in DB
+ *        tags:
+ *          - Flag
+ *        requestBody:
+ *          required: true
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  offset:
+ *                    type: integer
+ *                  limit:
+ *                    type: integer
+ *        responses:
+ *          200:
+ *            description: sucessfully gotten all flags
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  type: array
+ *                  items:
+ *                    type: object
+ *                    properties:
+ *                      id: 
+ *                        type: string
+ *                        format: uuid
+ *                      coordinates: 
+ *                        type: object
+ *                        properties:
+ *                          coordinates: 
+ *                            type: array
+ *                            items: 
+ *                              type: integer
+ *                            example: [0, 0]
+ *                          
+ *                      address: 
+ *                        type: string
+ *                      description:     
+ *                        type: string
+ *                      image: 
+ *                        type: string
+ *                      status: 
+ *                        type: string
+ *                        enum: ['PENDING', 'APPROVED', 'UNDER REVIEW', 'REJECTED']
+ *            500:
+ *              description: Error
+ *          
+ */
 module.exports.getAllFlags = (req, res) => {
   const { offset, limit } = req.body;
 
@@ -27,6 +82,63 @@ module.exports.getAllFlags = (req, res) => {
 };
 
 // get flags within a radius
+/**
+ * @swagger
+ *
+ *  paths:
+ *    /api/flag/getallinradius:
+ *      get:
+ *        description: Get All flags in the radius of user
+ *        tags:
+ *          - Flag
+ *        requestBody:
+ *          required: true
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  radius: 
+ *                    type: integer
+ *                  longitude: 
+ *                    type: number
+ *                  latitude:
+ *                     type: number
+ *        responses:
+ *          200:
+ *            description: sucessfully gotten all flags
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  type: array
+ *                  items:
+ *                    type: object
+ *                    properties:
+ *                      id: 
+ *                        type: string
+ *                        format: uuid
+ *                      coordinates: 
+ *                        type: object
+ *                        properties:
+ *                          coordinates: 
+ *                            type: array
+ *                            items: 
+ *                              type: integer
+ *                            example: [0, 0]
+ *                          
+ *                      address: 
+ *                        type: string
+ *                      description:     
+ *                        type: string
+ *                      image: 
+ *                        type: string
+ *                      status: 
+ *                        type: string
+ *                        enum: ['PENDING', 'APPROVED', 'UNDER REVIEW', 'REJECTED']
+ *            500:
+ *              description: Error
+ *          
+ */
 // TODO: debug this
 module.exports.getAllFlagsInRadius = async (req, res) => {
   const { radius, latitude, longitude } = req.body;
@@ -56,6 +168,63 @@ module.exports.getAllFlagsInRadius = async (req, res) => {
 };
 
 // create new flag
+/**
+ * @swagger
+ *
+ *  paths:
+ *    /api/flag/createflag:
+ *      post:
+ *        description: Create a new flag
+ *        tags:
+ *          - Flag
+ *        requestBody:
+ *          required: true
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  longitude: 
+ *                    type: number
+ *                  latitude:
+ *                     type: number
+ *                  description: 
+ *                     type: string
+ *                  image:
+ *                     type: string
+ *        responses:
+ *          200:
+ *            description: sucessfully created a new flag
+ *            content:
+ *              application/json:
+ *                schema:
+ *                   type: object
+ *                   properties:
+ *                     id: 
+ *                       type: string
+ *                       format: uuid
+ *                     coordinates: 
+ *                       type: object
+ *                       properties:
+ *                         coordinates: 
+ *                           type: array
+ *                           items: 
+ *                             type: integer
+ *                           example: [0, 0]
+ *                          
+ *                     address: 
+ *                       type: string
+ *                     description:     
+ *                       type: string
+ *                     image: 
+ *                       type: string
+ *                     status: 
+ *                       type: string
+ *                       enum: ['PENDING', 'APPROVED', 'UNDER REVIEW', 'REJECTED']
+ *            500:
+ *              description: Error
+ *          
+ */
 module.exports.createFlag = (req, res) => {
   // for now, send from frontend
   // after this, take the ID from JWT
@@ -99,6 +268,61 @@ module.exports.deleteFlag = (req, res) => {
     });
 };
 
+/**
+ * @swagger
+ *
+ *  paths:
+ *    /api/flag/getapproved:
+ *      get:
+ *        description: Get All Approved flags in DB
+ *        tags:
+ *          - Flag
+ *        requestBody:
+ *          required: true
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  offset:
+ *                    type: integer
+ *                  limit:
+ *                    type: integer
+ *        responses:
+ *          200:
+ *            description: sucessfully gotten all flags
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  type: array
+ *                  items:
+ *                    type: object
+ *                    properties:
+ *                      id: 
+ *                        type: string
+ *                        format: uuid
+ *                      coordinates: 
+ *                        type: object
+ *                        properties:
+ *                          coordinates: 
+ *                            type: array
+ *                            items: 
+ *                              type: integer
+ *                            example: [0, 0]
+ *                          
+ *                      address: 
+ *                        type: string
+ *                      description:     
+ *                        type: string
+ *                      image: 
+ *                        type: string
+ *                      status: 
+ *                        type: string
+ *                        enum: ['PENDING', 'APPROVED', 'UNDER REVIEW', 'REJECTED']
+ *            500:
+ *              description: Error
+ *          
+ */
 module.exports.getApprovedFlags = (req, res) => {
   const { offset, limit } = req.body;
 
