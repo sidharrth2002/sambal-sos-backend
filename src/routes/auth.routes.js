8f81e3bf2e05cbf77d526b7ab1d0b5b8c2b9f2c4
Sidharrth Nagappan <sidharrth2002@gmail.com>
2021-07-18 11:03:10 +0800

    facebook login complete on backend

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,9 +4,12 @@ const {
   login,
   googleLogin,
   validateRules,
+  facebookLogin,
 } = require("../controllers/auth.controller");
 const { validate } = require("../../utils/utils");
 
 router.post("/google", googleLogin);
+
+router.post("/facebook", facebookLogin);
 
 module.exports = router;
